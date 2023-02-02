ce1a3d26d22f8afabe0f434b1872e8a55079b557
Matheus da Silva Farias <71537027+MatthewsTomts@users.noreply.github.com>
2023-02-02 17:15:44 -0300

    Linhas e Colunas

--- Aula03_02-02/script.js
+++ Aula03_02-02/script.js
@@ -1,22 +1,74 @@
-var linha = 1
-var notas = 1
+var linQtd = 1
+var notasQtd = 1
 
-function adicionar() {
-    var corpo = document.getElementById('tbody')
-    linha += 1
-    if (linha > 10) {
-        alert('Limite de linhas')
+function addLin() {
+    if (linQtd < 10){
+        linQtd += 1
     } else {
-        corpo.innerHTML += `
-        <tr>
-            <th>${linha}</th>
-            <td><input type="text" class="form-control" id="" placeholder="nome"></td>
-            <td><input type="number" class="form-control" id="" placeholder=""></td>
-            <td><input type="number" class="form-control" id="" placeholder=""></td>
-            <td><input type="number" class="form-control" id="" placeholder=""></td>
-            <td><input type="number" class="form-control" id="" placeholder=""></td>
-            <td><output></output></td>
-            <td><output></output></td>
-        </tr>`
+        alert('Limite de linhas atingido.')
     }
+    montarTabela()
+}
+
+function addNotas() {
+    if (notasQtd < 6){
+        notasQtd += 1
+    } else {
+        alert('Limite de notas atingido.')
+    }
+    montarTabela()
+}
+
+function montarTabela() {
+    var tabela = document.getElementById('tabela')
+    tabela.innerHTML = `
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        ${montarHeadNotas()}
+        <th scope="col">Média</th>
+        <th scope="col">Situação</th>
+        </tr>
+    </thead>
+    <tbody id="tbody">
+        ${montarLin()}
+    </tbody>
+    `
+}
+
+function montarHeadNotas() {
+    var notas = ''
+    for(i = 1; i <= notasQtd; i++) {
+        notas += `<th scope='col'>Nota ${i}</th>`
+    }
+    return notas
+}
+
+function montarLin() {
+    var linhas = ''
+    for(i = 1; i <= linQtd; i++) {
+        linhas += `
+        <tr>
+        <th>${i}</th>
+        <td><input type="text" class="form-control" id="" placeholder="nome"></td>
+        ${montarNotaLin()}
+        <td><output></output></td>
+        <td><output></output></td>
+        </tr>
+        `
+    }
+    return linhas
+}
+
+function montarNotaLin() {
+    var notasLin = ''
+    for(j = 0; j < notasQtd; j++) {
+        notasLin += '<td><input type="number" class="form-control" id="" placeholder=""></td>'
+    }
+    return notasLin
+}
+
+function calcular() {
+    
 }
