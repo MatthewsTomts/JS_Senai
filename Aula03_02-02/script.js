0710d03ecc1af8919b77726438b0eb9b7c6b0bfe
MatthewsTomts <learningahh@gmail.com>
2023-02-05 20:52:18 -0300

    Ordem cres/decres

--- Aula03_02-02/script.js
+++ Aula03_02-02/script.js
@@ -1,11 +1,11 @@
 var linQtd = 1
 var notasQtd = 1
 var medias = ['']
-var ordNum = 0
+var ordem = -1
 
-function montarTabela(ordAlph = false, ordNum = 0) {
+function montarTabela(ordem) {
     var tabela = document.getElementById('tabela')
-    dados = datasLin(ordAlph, ordNum)
+    dados = datasLin(ordem)
     tabela.innerHTML = `
     <thead>
         <tr>
@@ -20,6 +20,7 @@ function montarTabela(ordAlph = false, ordNum = 0) {
         ${montarLin(medias, dados[0], dados[1])}
         <td colspan="${notasQtd + 4}"><output>A média geral da sala: <br>Geral: ${mediaGeral(medias)}</output></td>
     </tbody>
+    <p id="odd" hidden>${ordem}</p>
     `
 }
 
@@ -31,7 +32,7 @@ function montarHeadNotas() {
     return notas
 }
 
-function datasLin(ordAlph, ordNum) {
+function datasLin(ordem) {
     var nomes = []
     var notasGeral = []
 
@@ -55,30 +56,22 @@ function datasLin(ordAlph, ordNum) {
         notasGeral.push(notas)
     }
     
-    if (ordAlph) {
-        var lista = [];
-        for (var j = 0; j < nomes.length; j++) 
-            lista.push({'nome': nomes[j], 'nota': notasGeral[j]});
-
+    var lista = [];
+    for (var j = 0; j < nomes.length; j++) 
+        lista.push({'nome': nomes[j], 'nota': notasGeral[j], 'media': medias[j]});
+    if (ordem == 2) {
         lista.sort(function(a, b) {
             return ((a.nome < b.nome) ? -1 : ((a.nome == b.nome) ? 0 : 1));
         });
-
-        for (var k = 0; k < lista.length; k++) {
-            nomes[k] = lista[k].nome;
-            notasGeral[k] = lista[k].nota;
-        }
-    } else if (ordNum != 0) {
-        // var lista = [];
-        // for (var j = 0; j < nomes.length; j++) 
-        //     lista.push({'nome': nomes[j], 'nota': notasGeral[j]});
-
-        
-
-        // for (var k = 0; k < lista.length; k++) {
-        //     nomes[k] = lista[k].nome;
-        //     notasGeral[k] = lista[k].nota;
-        // }
+    } else if (ordem != 0) {
+        lista.sort(function(a, b) {
+            return (a.media - b.media) * ordem;
+        });
+    }
+    for (var k = 0; k < lista.length; k++) {
+        nomes[k] = lista[k].nome;
+        notasGeral[k] = lista[k].nota;
+        medias[k] = lista[k].media;
     }
     return [nomes, notasGeral]
 }
@@ -116,7 +109,7 @@ function addLin() {
     } else {
         alert('Limite de linhas atingido.')
     }
-    montarTabela()
+    montarTabela(ordem)
 }
 
 function addNotas() {
@@ -125,7 +118,11 @@ function addNotas() {
     } else {
         alert('Limite de notas atingido.')
     }
-    montarTabela()
+    medias = []
+    for (i = 0; i < notasQtd; i++) {
+        medias.push('')
+    }
+    montarTabela(ordem)
 }
 
 function delLin() {
@@ -135,7 +132,7 @@ function delLin() {
     } else {
         alert('Limite de linhas atingido.')
     }
-    montarTabela()
+    montarTabela(ordem)
 }
 
 function delNotas() {
@@ -144,7 +141,7 @@ function delNotas() {
     } else {
         alert('Limite de notas atingido.')
     }
-    montarTabela()
+    montarTabela(ordem)
 }
 
 function calcular() {
@@ -161,7 +158,7 @@ function calcular() {
             medias.splice(i, 0, med.toFixed(2))
         }
     }
-    montarTabela()
+    montarTabela(ordem)
 }
 
 function situ(media) {
@@ -189,13 +186,16 @@ function mediaGeral(medias) {
 }
 
 function ordemAlph() {
-    montarTabela(true)
+    ordem = 2
+    montarTabela(ordem)
 }
 
 function ordemNum() {
-    if (ordNum == 1) {
-        montarTabela(ordNum = -1)
-    } else {
-        montarTabela(ordNum = 1)
+    ordem = parseInt(document.getElementById("odd").innerText) * -1
+    if (ordem == 2) {
+        ordem -= 1
+    } else if (ordem == -2) {
+        ordem += 1
     }
+    montarTabela(ordem)
 }
